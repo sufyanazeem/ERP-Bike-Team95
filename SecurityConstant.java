d5cc20836f75704789fedb79e24f236d93a0c20d
sufyanazeem <88711981+sufyanazeem@users.noreply.github.com>
2023-03-16 23:01:53 +0500

    Add files via upload

---
+++ SecurityConstant.java
@@ -0,0 +1,13 @@
+package com.soen390.team11.security;
+
+/**
+ * Constants used in the Security Layer
+ */
+public class SecurityConstant {
+    public static final String SIGN_UP_URLS = "/account/signup";
+    public static final String SIGN_IN_URLS = "/account/signin";
+    public static final String SECRET = "SecretKeyToGenJWTS";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 86400000; // in millisecond one day
+}
